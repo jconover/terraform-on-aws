bbb2833cafd58287342e4d5e75cb33162e8840a3
Justin Conover <justin.conover@gmail.com>
2025-08-25 20:44:43 -0500

    cleanup on main.tf

--- aws-ec2/jenkins/main.tf
+++ aws-ec2/jenkins/main.tf
@@ -128,10 +128,10 @@ resource "aws_security_group" "demo_sg" {
 
   # All outbound traffic
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
@@ -153,7 +153,7 @@ resource "aws_instance" "demo-server" {
   key_name               = aws_key_pair.demo_deployer.key_name
   vpc_security_group_ids = [aws_security_group.demo_sg.id]
   subnet_id              = aws_subnet.demo_public-subnet-01.id
-  for_each = toset(["jenkins-master", "jenkins-slave", "ansible"])
+  for_each               = toset(["jenkins-master", "jenkins-slave", "ansible"])
 
   user_data = <<-EOF
               #!/bin/bash
